d9f6c573547514eb6fbd1d220695c1fa86c933f7
laurenfazah <laurenfazah@gmail.com>
2016-10-29 10:39:29 -0600

    Initial commit

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+Encoding.default_external = "utf-8"
+http_path = "/"
+css_dir = "html/stylesheets"
+sass_dir = "build/sass"
+images_dir = "html/img"
+javascripts_dir = "html/js"
+output_style = :compressed
+relative_assets = true
+http_images_dir = "html/img"
+sourcemap = true
